fa8147b524cf38f9e5d27f27cf2d749e665780fd
nor <haizzzzzzz123123@gmail.com>
2026-05-27 07:58:31 +0700

    fix(auth): fix type error for setInterval on mobile

--- app/forgot-password.tsx
+++ app/forgot-password.tsx
@@ -67,7 +67,7 @@ export default function ForgotPasswordScreen() {
   const { showAlert } = useAlertStore();
 
   useEffect(() => {
-    let interval: NodeJS.Timeout;
+    let interval: any;
     if (countdown > 0) {
       interval = setInterval(() => {
         setCountdown((prev) => prev - 1);
